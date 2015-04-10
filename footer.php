111075489c5d018b3f7371f757617d211bce5fe4
jlv <jlvieitez@gmail.com>
2015-04-10 08:37:35 -0430

    Datos de contácto en el footer

--- footer.php
+++ footer.php
@@ -7,6 +7,11 @@
 						<div class="juventud">
 							<img src="<?php echo get_template_directory_uri() ?>/static/images/logo-pueblo-victorioso.png" alt="Pueblo Victorioso">
 						</div>
+						<div class="info">
+							<span>Esquina de Aserradero, intercepción de las Calles Sur 8 y Oeste 6, Parroquia Catedral, Municipio Libertador del Distrito Capital</span>
+							<span>(0212) 377 3198 / 377 3198 | (0416) 623 5368 / (0416) 623 5358</span>
+							<span>contacto@amazoniafilms.gob.ve</span>
+						</div>
 					</div>
 				</footer>
 			</div>
